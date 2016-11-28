17b7c1a28a56f140250d49addfd69f54e7c0c82b
Chris Clifford and Ezra Sandzer-Bell <ezra@>
2016-11-28 11:38:10 -0800

    adds code for multiple entries in the hash using arrays and passes spec

--- spec/hashtest_spec.rb
+++ spec/hashtest_spec.rb
@@ -2,11 +2,21 @@ require('rspec')
 require('hashtest')
 
 describe(Hashtester) do
-  describe('#store') do
+  describe('#fetch') do
     it('stores key and value') do
       demo_hash = Hashtester.new()
       demo_hash.store("test", "output")
       expect(demo_hash.fetch("test")).to(eq("output"))
     end
   end
+
+  describe('#fetch') do
+    it('fetches key value') do
+      demo_hash = Hashtester.new()
+      demo_hash.store("test", "output")
+      demo_hash.store("test again", "output again")
+      expect(demo_hash.fetch("test again")).to(eq("output again"))
+    end
+  end
+
 end
